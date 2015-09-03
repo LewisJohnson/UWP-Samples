eaef65915d6416513887d2e826a0090716a4d2a0
Lewis Johnson <lewisjohnsondev@gmail.com>
2015-09-03 14:37:34 +0100

    Shimmer

--- AppMe/Examples/Shimmer.xaml.cs
+++ AppMe/Examples/Shimmer.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +15,15 @@ namespace AppMe.Examples
         public Shimmer()
         {
             this.InitializeComponent();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(0.5));
+                ShimmerAnimation.Begin();
+            }
         }
     }
 }
--- AppMe/Examples/StripesBackgroundFill.xaml.cs
+++ AppMe/Examples/StripesBackgroundFill.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,8 +20,10 @@ namespace AppMe.Examples
 
         private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
         {
-            BackgroundFill.RepeatBehavior = RepeatBehavior.Forever;
-            BackgroundFill.Begin();
+            BackgroundAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            BackgroundAnimation2.RepeatBehavior = RepeatBehavior.Forever;
+            BackgroundAnimation.Begin();
+            BackgroundAnimation2.Begin();
         }
     }
 }
